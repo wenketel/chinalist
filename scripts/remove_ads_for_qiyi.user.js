0630458a453ff402e5d7f1b94320ab25a674e371
gythialy.koo@gmail.com <gythialy.koo@gmail.com@438db56c-134b-0410-a3ef-09048d3a9d03>
2011-06-16 12:49:43 +0000

    add greasemonkey script for qiyi.com
    git-svn-id: http://adblock-chinalist.googlecode.com/svn/trunk@3439 438db56c-134b-0410-a3ef-09048d3a9d03

---
+++ scripts/remove_ads_for_qiyi.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name Qiyi ads Remover
+// @author Gythialy
+// @description  Remove qiyi.com flash ads for ChinaList
+// @create 2011-6-16
+// @lastmodified 2011-6-16
+// @version 1.0.1
+// @updatelog ....
+// @namespace  http://code.google.com/p/adblock-chinalist/
+// @include http://www.qiyi.com/*
+// @include http://yule.qiyi.com/*
+// ==/UserScript==
+
+(function() {
+	function getQCookie(name) {
+		var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+		if (arr != null)
+			return unescape(arr[2]);
+
+		return null;
+	}
+
+	function setQCookie(name, value, expire) {
+		var crazy_exp = new Date();
+		crazy_exp.setTime(crazy_exp.getTime() + expire * 24 * 60 * 60 * 1000);
+		document.cookie = name + "=" + value + ";expires=" + crazy_exp.toGMTString();
+	}
+
+	function closeCrazy() {
+		var jdtflash = document.getElementById('jdtflash');
+		if (jdtflash)
+			jdtflash.style.display = '';
+		var adflash = document.getElementById('adflash');
+		if (adflash)
+			adflash.style.display = 'none';
+	}
+
+	closeCrazy();
+
+	var protocol = window.location.protocol.toString();
+	var url = window.location.href.toString().substring(protocol.length);
+
+	var qycrazy = getQCookie('qycrazy');
+	qycrazy = qycrazy == null ? 0 : qycrazy;
+	if (qycrazy < 2 && url.indexOf('www') != -1) {
+		setQCookie('qycrazy', 2, 1);
+	}
+
+	var qyylcrazy = getQCookie('qyylcrazy');
+	qyylcrazy = qyylcrazy == null ? 0 : qycrazy;
+	if (qycrazy < 2 && url.indexOf('yule') != -1) {
+		setQCookie('qyylcrazy', 2, 1);
+	}
+})();
